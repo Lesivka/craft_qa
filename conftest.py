6c9aedcdb3f571cf8c361d7b0cee6abd3d886009
bsa <bsa@stacksoft.ru>
2023-04-14 01:30:46 +0300

    Added base_page.py, conftest.py, requirements.txt

---
+++ conftest.py
@@ -0,0 +1,13 @@
+# Фикстура для запуска вебдрайвера, setup - этап формирования теста, tierdown - завершение теста
+import pytest
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+@pytest.fixture(scope='function')
+def driver():
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.maximize_window()
+
+    yield driver
+    driver.quit()
